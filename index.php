5a9438c19e7fa7f8b4db55dd419dcc1db4837046
portable_ludo <rigoludo@gmail.com>
2018-11-28 16:30:19 +0100

    index.php fonctionnelle

--- index.php
+++ index.php
@@ -4,4 +4,21 @@ if(isset($_POST["operation"])) {
     header("Location: /TP 28-11/operations.php");
         die;
 }
-?>
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Accueil</title>
+</head>
+<body>
+    <header>
+        <h1>Accueil</h1>
+    </header>
+    <form action="" method="post">
+        Mes opérations --> 
+        <button type="submit" name="operation">OK</button>
+    </form>
+</body>
+</html>
