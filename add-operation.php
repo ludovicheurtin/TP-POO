4fd02962fd7c6ffb4079fc0702a50f864bd016e7
Eva Guegan <eva.guegan@hotmail.fr>
2018-11-29 12:19:42 +0100

    modif test

--- add-operation.php
+++ add-operation.php
@@ -1,23 +1,31 @@
 <?php
 session_start();
  
-// tout les form soit complet sinon pas redirection
- 
-$champs=true;
-$label=true;
-$montant=true;
+$champs=false;
+$label=false;
+$montant=false;
  
 
 //Verifier si le formulaire est complet
-if(empty($_POST["label"]) or (empty($_POST["montant"]))) {
+if(!empty($_POST["label"]) && (!empty($_POST["montant"]))) {
     echo "Les champs sont obligatoire";
-    $champs=false;
- 
-}else{
     $champs=true;
+    $label=true;
+    $montant=true;
+}
+
+var_dump($_POST);
+var_dump($champs);
+var_dump($label);
+var_dump($montant);
+
+
+// Quand formulaire est complet
+if (($champs===true) && ($label===true) && ($montant===true)) {
     header("location: index.php");
 }
- 
+
+
 ?><!DOCTYPE html>
 <html lang="fr">
     <head>
@@ -30,7 +38,7 @@ if(empty($_POST["label"]) or (empty($_POST["montant"]))) {
             <h1>Ajouter une opération</h1>
         </header>
  
-        <form action="" method="post">
+        <form action="" method="POST">
             <div>
                 <input type="text" name="label" placeholder="Saisir un label">
             </div>
@@ -39,8 +47,7 @@ if(empty($_POST["label"]) or (empty($_POST["montant"]))) {
                 <input type="text" name="montant" placeholder="Saisir un montant">
             </div>
         </form>
-        
-        <button type="submit">OK</button>
- 
+
+        <button type="submit" name="btn">OK</button>
     </body>
 </html>
